0286ab3ca2bc89d38a321c7fb51539cea6cb49e1
Harry Mamba <harry.mamba@hiveempire.com>
2016-08-11 08:44:32 +0800

    riot-class

---
+++ src/api_key.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyB82kIaVGxDeQnlDJbbMMm0j2MQwpCrN-s';
+
+export default API_KEY;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const API_KEY = 'AIzaSyAUFwoyiCLhzsYLJo8KDmSLeqaPZUiaPZQ';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+import _ from 'lodash';
+import API_KEY from './api_key';
 
 // Create component that should produce some HTML
-const App = () => {
-	return <div>Hi!</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0] 
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList 
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
 // Generate HTML and put it on page DOM 
